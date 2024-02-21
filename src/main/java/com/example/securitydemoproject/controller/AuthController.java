2ffcea26e05a52b1d839656b6c2439304cdb95c1
seoyoon <hi.seoyoon22@gmail.com>
2024-02-21 22:46:48 +0900

    030: Add logging to controller methods

--- src/main/java/com/example/securitydemoproject/controller/AuthController.java
+++ src/main/java/com/example/securitydemoproject/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.example.securitydemoproject.dto.JwtRequestDto;
 import com.example.securitydemoproject.dto.MemberSignupRequestDto;
 import com.example.securitydemoproject.service.AuthService;
 import com.example.securitydemoproject.service.LoginHistoryService;
+import com.example.securitydemoproject.util.LoggerUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
@@ -27,15 +28,19 @@ public class AuthController {
     @PostMapping(value = "/login")
     @ApiOperation(value = "Authenticate user and generate JWT token")
     public ResponseEntity<String> login(@RequestBody JwtRequestDto request) {
+        LoggerUtil.logInfo(AuthController.class, "Request received to login user with email: "+request.getEmail());
         String token = authService.login(request);
         loginHistoryService.logLoginHistory(request.getEmail());
+        LoggerUtil.logInfo(AuthController.class, "User logged in successfully.");
         return ResponseEntity.ok(token);
     }
 
     @PostMapping(value = "/signup")
     @ApiOperation(value = "Register a new user")
     public ResponseEntity<String> signup(@RequestBody MemberSignupRequestDto request) {
+        LoggerUtil.logInfo(AuthController.class, "Request received to signup new user with email: "+ request.getEmail());
         String message = authService.signup(request);
+        LoggerUtil.logInfo(AuthController.class, "New user signed up successfully.");
         return ResponseEntity.ok(message);
     }
 
@@ -44,6 +49,7 @@ public class AuthController {
     public ResponseEntity<String> logout(HttpServletRequest request) {
         String token = authService.extractTokenFromRequest(request);
         authService.logout(token);
+        LoggerUtil.logInfo(AuthController.class, "User logged out successfully.");
         return ResponseEntity.ok("Logout successful");
     }
 }
--- src/main/java/com/example/securitydemoproject/controller/UserController.java
+++ src/main/java/com/example/securitydemoproject/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.securitydemoproject.controller;
 import com.example.securitydemoproject.dto.ChangePasswordRequest;
 import com.example.securitydemoproject.security.JwtProvider;
 import com.example.securitydemoproject.service.UserService;
+import com.example.securitydemoproject.util.LoggerUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,9 @@ public class UserController {
     @ApiOperation(value = "Get user details")
     public ResponseEntity<Map<String, Object>> getUser(HttpServletRequest request) {
         String username = jwtProvider.getUsernameFromToken(jwtProvider.resolveToken(request));
+        LoggerUtil.logInfo(UserController.class, "Request received to get details for user " + username);
         Map<String, Object> response = userService.getUserByUserName(username);
+        LoggerUtil.logInfo(UserController.class, "User details retrieved successfully.");
         return ResponseEntity.ok(response);
     }
 
@@ -37,7 +40,9 @@ public class UserController {
     @ApiOperation(value = "Change user password")
     public ResponseEntity<String> changePassword(HttpServletRequest request, @RequestBody ChangePasswordRequest newPassword) {
         String username = jwtProvider.getUsernameFromToken(jwtProvider.resolveToken(request));
+        LoggerUtil.logInfo(UserController.class, "Request received to change password for user " + username);
         userService.changePassword(username, newPassword.getNewPassword());
+        LoggerUtil.logInfo(UserController.class, "Password changed successfully for user " + username);
         return ResponseEntity.ok("Password changed successfully");
     }
 }
