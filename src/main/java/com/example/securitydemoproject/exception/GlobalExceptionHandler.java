85a8d3f107dce9d2de29ce05695f717bc31ec85e
seoyoon <hi.seoyoon22@gmail.com>
2024-03-01 15:32:59 +0900

    041: Eliminate duplicate code in GlobalExceptionHand

--- src/main/java/com/example/securitydemoproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/securitydemoproject/exception/GlobalExceptionHandler.java
@@ -1,54 +1,20 @@
 package com.example.securitydemoproject.exception;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.security.InvalidKeyException;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(DataAccessException.class)
-    public ResponseEntity<Object> handleDatabaseExceptions(DataAccessException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while accessing the database: " + ex.getMessage());
-    }
-
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+    public ResponseEntity<Map<String, Object>> handleDatabaseExceptions(DataAccessException ex) {
         Map<String, Object> errorResponse = createErrorResponse(ex);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-    }
-
-    @ExceptionHandler(DuplicateKeyException.class)
-    public ResponseEntity<Map<String, Object>> handleDuplicateKeyException(DuplicateKeyException ex) {
-        Map<String, Object> errorResponse = createErrorResponse(ex);
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
-    }
-
-    @ExceptionHandler(InvalidKeyException.class)
-    public ResponseEntity<Map<String, Object>> handleInvalidKeyException(InvalidKeyException ex) {
-        Map<String, Object> errorResponse = createErrorResponse(ex);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<Map<String, Object>> handleBadCredentialsException(BadCredentialsException ex) {
-        Map<String, Object> errorResponse = createErrorResponse(ex);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    }
-
-    @ExceptionHandler(AuthenticationServiceException.class)
-    public ResponseEntity<Map<String, Object>> AuthenticationServiceException(AuthenticationServiceException ex) {
-        Map<String, Object> errorResponse = createErrorResponse(ex);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
